c261445f9a2195f2a0a456d68ba90f5e8e5aeae3
Obinna Ezekwe <ozekwe@gmail.com>
2020-12-07 16:04:29 -0600

    progress

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
@@ -51,3 +56,13 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
