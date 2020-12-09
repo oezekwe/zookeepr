db5b2c2cf555308e7dc1cc5eba35d8f6bc744802
Obinna Ezekwe <ozekwe@gmail.com>
2020-12-08 18:55:03 -0600

    complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app= express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 
 function validateAnimal(animal) {
@@ -77,6 +78,18 @@ function createNewAnimal(body, animalsArray) {
   return animal;
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
@@ -110,4 +123,9 @@ app.post('/api/animals', (req, res) => {
       res.json(animal);
     }
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
